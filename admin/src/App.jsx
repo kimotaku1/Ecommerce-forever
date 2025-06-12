3e410026a0f79d5e949ba6382903cdbbd5ddbea5
Bibek Bhandari <narutobibek000@gmail.com>
2025-06-12 16:32:07 +0545

    scroll button removed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,6 @@ import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
-import ScrollToTopButton from "./components/ScrollToTopButton";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const currency = "Rs.";
@@ -24,7 +23,7 @@ const App = () => {
     localStorage.setItem("token", token);
   }, [token]);
   return (
-    <div className="bg-gray-50 min-h-screen overflow-hidden">
+    <div className="bg-gray-50 min-h-screen">
       <ToastContainer />
       {token === "" ? (
         <Login setToken={setToken} />
@@ -34,7 +33,7 @@ const App = () => {
           <hr />
           <div className="flex w-full">
             <Sidebar />
-            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base overflow-y-auto max-h-[calc(100vh-100px)] pr-2">
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
                 <Route path="/add" element={<Add token={token} />} />
                 <Route path="/list" element={<List token={token} />} />
@@ -42,7 +41,6 @@ const App = () => {
               </Routes>
             </div>
           </div>
-          <ScrollToTopButton />
         </>
       )}
     </div>
